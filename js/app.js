b95e9f8bae4249d7f0138590cdd9e67e2e56b014
OscarSebastianRuedaRojas <152028425+OscarSebastianRuedaRojas@users.noreply.github.com>
2024-02-27 18:13:48 -0500

    feat: :sparkles: Creacion Dialogs
    Se crearon la funcion que realiza la aparicion y creacion de los dialog, y el cambio de franquisia en el html

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Importacion del archivo Json
 const fetchData = async () =>{
   try{
     const res = await fetch("../storage/data/heroes.json")
@@ -9,10 +10,15 @@ const fetchData = async () =>{
     console.error(error);
   }
 }
-const heroes = await fetchData()
-const container = document.querySelector(".containerMarvel")
+//Declaracion de variables
+const heroes = await fetchData();
+const container = document.querySelector(".containerMarvel");
+const container2 = document.querySelector(".containerDc");
+const logoMarvel = document.querySelector("#logoMarvel");
+const logoDc = document.querySelector("#logoDc");
+//Creaacion de cards
 const crearTarjeta = (picture, name)=>{
-    const card = document.createElement("div");
+    const card = document.createElement("article");
     const img = document.createElement("img");
     const div = document.createElement("div");
     const div2 = document.createElement("div");
@@ -23,6 +29,7 @@ const crearTarjeta = (picture, name)=>{
     const boton = document.createElement("button")
     boton.innerHTML = "<span>Ver</span>";
     const nameHeroe = document.createElement("h2");
+    boton.setAttribute("id", name);
     nameHeroe.innerText = name;
     div2.appendChild(nameHeroe);
     div2.appendChild(boton);
@@ -31,8 +38,88 @@ const crearTarjeta = (picture, name)=>{
     card.appendChild(div2);
     return card
 }
-console.log(heroes);
+//Creacion de Dialog
+const preview = (name, picture, about) =>{
+  const blur = document.createElement("section")
+  blur.setAttribute("class", "blurname");
+  const dialog = document.createElement("dialog")
+  const img = document.createElement("img")
+  img.src = picture;
+  img.alt = "Imagen de superheroe no disponible";
+  const div = document.createElement("div")
+  const nameHeroe = document.createElement("h2");
+  nameHeroe.innerText = name;
+  const description = document.createElement("p")
+  description.innerText = about
+  const close = document.createElement("button");
+  close.classList.add('tal')
+  close.innerHTML = "<i class='bx bx-x'></i>";
+  div.appendChild(nameHeroe);
+  div.appendChild(description);
+  div.appendChild(close);
+  dialog.appendChild(img);
+  dialog.appendChild(div);
+  blur.appendChild(dialog);
+  return blur
+}
+//Invocacion del Dialog
+/**
+ * 
+ * @param {Array} heroes 
+ */
+const createButton = (heroes) =>{
+  let botones = document.querySelectorAll("button");
+  botones.forEach(boton => {
+    boton.addEventListener("click", ()=>{
+      const name = boton.id
+      let ind = heroes.findIndex(hero => hero.name === name)
+      let verificacion = document.querySelector("dialog")
+      if(verificacion){
+        console.log("Ya existe un dialog");
+      }
+      else{
+        let dialog = preview(heroes[ind].name , heroes[ind].picture , heroes[ind].about)
+        dialog.open
+        document.body.appendChild(dialog)
+        if(dialog){
+          deletePreview()
+        }
+      }
+    })
+  });
+}
+//Agregacion de Tarjetas segun datos de JSon
 heroes.marvel.forEach(heroe => {
     let elementCreado = crearTarjeta(heroe.picture, heroe.name);
     container.appendChild(elementCreado)
 });
+heroes.dc.forEach(heroe =>{
+    let elementCreado = crearTarjeta(heroe.picture, heroe.name);
+    container2.appendChild(elementCreado)
+})
+//Eliminacion de Dialogs
+const deletePreview = () =>{
+  const closes = document.querySelector(".tal")
+        closes.addEventListener('click', () =>{
+            let body = document.querySelector("body")
+            let dialogEliminar = document.querySelector(".blurname")
+            body.removeChild(dialogEliminar)
+          })
+}
+createButton(heroes.marvel);
+//Funcion Para cambio de franquisia
+const cambioFranquisia = (logo, container, container2)=>{
+  logo.addEventListener('click',()=>{
+    if (container.style.display !=='none'){
+      container.style.display = 'none'
+      container2.style.display = 'grid'
+    }
+    if (container.style.display !=='none'){
+      createButton(heroes.marvel);
+    }else{
+      createButton(heroes.dc);
+    }
+  })
+}
+cambioFranquisia(logoMarvel, container2, container);
+cambioFranquisia(logoDc, container, container2);
